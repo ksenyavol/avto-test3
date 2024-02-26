32e9ef6abb8177d76ac2564c43ad5cde8e9a476e
macbook <KpuBopy4Ka86@gmail.com>
2024-02-26 14:43:09 +0300

    Семинар 3

---
+++ src/test/java/org/max/lesson3/home/accuweather/GetWeatherFiveDayTest.java
@@ -0,0 +1,28 @@
+package org.max.lesson3.home.accuweather;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.max.lesson3.home.accuweather.weather.Weather;
+
+import static io.restassured.RestAssured.given;
+
+public class GetWeatherFiveDayTest extends AccuweatherAbstractTest {
+
+    @Test
+    void getWeatherFiveDay_shouldReturn() {
+
+        Weather response = given()
+                .queryParam("apikey", getApiKey())
+                .when()
+                .get(getBaseUrl()+"/forecasts/v1/daily/5day/290396")
+                .then()
+                .statusCode(200)
+                .time(Matchers.lessThan(2000l))
+                .extract()
+                .response()
+                .body().as(Weather.class);
+
+        Assertions.assertEquals(5,response.getDailyForecasts().size());
+    }
+}
